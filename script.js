fe07a01549183e50c4996a152aaf1c185ba8580a
Blessed Deep <79470320+Blessed-Deep@users.noreply.github.com>
2022-02-15 12:07:45 +0530

    updated searchpen

--- script.js
+++ script.js
@@ -12,64 +12,40 @@ window.addEventListener('DOMContentLoaded',() => {
     
     const triggerOnOff =function(){ 
         if(pentrigger.classList.contains('on')){                        
-            pentrigger.classList.remove("on");                        
-            pentrigger.classList.toggle("off");
-
-            searchicostick.classList.remove("icostickOn");
-            searchicostick.classList.toggle("icostickOff");
-
-            searchICO.classList.remove("searchicoOn");
-            searchICO.classList.toggle("searchicoOff");
-
-            stick1.classList.remove("s1AniOn");
-            stick1.classList.toggle("s1AniOff");
-
-            stick2.classList.remove("s2AniOn");
-            stick2.classList.toggle("s2AniOff");
-
-            RadioBtn.classList.remove("radioOn");
-            RadioBtn.classList.toggle("radioOff");
-
-            searchArea.classList.remove("searchAreaOn");
-            searchArea.classList.toggle("searchAreaOff");
-
-            EditText.classList.remove("EditTextOn");
-            EditText.classList.toggle("EditTextOff");
-
-            
-
-
-            
+                OffICO();
         } else {                        
-            pentrigger.classList.remove("off");
-            pentrigger.classList.toggle("on");
-
-            searchicostick.classList.remove("icostickOff");
-            searchicostick.classList.toggle("icostickOn");
-           
-            searchICO.classList.remove("searchicoOff");
-            searchICO.classList.toggle("searchicoOn");
-
-            stick1.classList.remove("s1AniOff");
-            stick1.classList.toggle("s1AniOn");
-
-            stick2.classList.remove("s2AniOff");
-            stick2.classList.toggle("s2AniOn");
-
-            RadioBtn.classList.remove("radioOff");
-            RadioBtn.classList.toggle("radioOn");
-
-            searchArea.classList.remove("searchAreaOff");
-            searchArea.classList.toggle("searchAreaOn");
-
-            EditText.classList.remove("EditTextOff");
-            EditText.classList.toggle("EditTextOn");
+            OnICO();
         }
     }
     pentrigger.addEventListener('click',triggerOnOff)
+   
+    const OffICO =function(){
+        pentrigger.classList.remove("on");                        
+        pentrigger.classList.toggle("off");
+
+        searchicostick.classList.remove("icostickOn");
+        searchicostick.classList.toggle("icostickOff");
+
+        searchICO.classList.remove("searchicoOn");
+        searchICO.classList.toggle("searchicoOff");
+
+        stick1.classList.remove("s1AniOn");
+        stick1.classList.toggle("s1AniOff");
+
+        stick2.classList.remove("s2AniOn");
+        stick2.classList.toggle("s2AniOff");
+
+        RadioBtn.classList.remove("radioOn");
+        RadioBtn.classList.toggle("radioOff");
+
+        searchArea.classList.remove("searchAreaOn");
+        searchArea.classList.toggle("searchAreaOff");
+
+        EditText.classList.remove("EditTextOn");
+        EditText.classList.toggle("EditTextOff");
+    }
 
     const OnICO =function(){ 
-            //   Here an Exception can occure i will fix it later 
             pentrigger.classList.remove("off");
             pentrigger.classList.toggle("on");
 
@@ -95,7 +71,24 @@ window.addEventListener('DOMContentLoaded',() => {
             EditText.classList.toggle("EditTextOn");
                        
     }
-    OnOffICO.addEventListener('click', OnICO)
-    OnICO();
+    OnOffICO.addEventListener('click', OnICO);
+    
    
+    (function(){
+        var c = 0; 
+        window.onresize = calWindowSize;
+        window.onload = calWindowSize;
+        function calWindowSize(){
+            var myWidth = screen.width;
+            
+  
+            if((myWidth>800) && c==0){
+                c=1;
+                OnICO();
+            } else if(myWidth<=800 && c==1){
+                c=0;
+               OffICO();
+            }
+        }
+    })();
 })
